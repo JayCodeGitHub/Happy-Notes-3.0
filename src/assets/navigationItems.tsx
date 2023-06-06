150e13ef8d30dd7b5769d9f16a2ca3fee8a0daaa
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-06 19:50:41 +0200

    Moved navigation items to separate file

---
+++ src/assets/navigationItems.tsx
@@ -0,0 +1,7 @@
+export const NavigationItems = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Notes", href: "/notes" },
+  { name: "Todos", href: "/todo" },
+  { name: "Sites", href: "/sites" },
+];
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,16 @@
 import Link from "next/link";
+import { NavigationItems } from "@/assets/navigationItems";
 import DarkModeToggle from "./darkModeToggle";
 
 export default function NavBar() {
   return (
-    <nav className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
+    <nav className="text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <ul>
-        <Link href="/">
-          <li>Home</li>
-        </Link>
-        <Link href="/about">
-          <li>About</li>
-        </Link>
-        <Link href="/notes">
-          <li>Notes</li>
-        </Link>
-        <Link href="/todo">
-          <li>ToDo</li>
-        </Link>
-        <Link href="/sites">
-          <li>Sites</li>
-        </Link>
+        {NavigationItems.map(({ name, href }) => (
+          <Link href={href} key={name}>
+            <li>{name}</li>
+          </Link>
+        ))}
       </ul>
       <DarkModeToggle>Dark Mode</DarkModeToggle>
     </nav>
