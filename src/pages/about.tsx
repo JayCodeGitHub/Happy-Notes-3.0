7cdd9b9d2959500e8c8546f8d8811ec2393aee77
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 01:06:37 +0200

    Styled About page for desktop

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,18 @@
 export default function About() {
   return (
-    <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
-      <h1>About</h1>
+    <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 transition-all bg-slate-100 dark:bg-gray-900">
+      <section className="w-1/2 px-0 mx-auto mt-16 ">
+        <h1 className="mb-12 text-3xl text-gray-900 transition-all dark:text-white">
+          About
+        </h1>
+        <p className="text-gray-600 transition-all text-md md:text-xl dark:text-gray-400">
+          To-do list app designed for productivity enthusiasts. With seamless
+          functionality, this application allows users to effortlessly create
+          and manage their tasks. To delete an item, simply swipe right across
+          the screen. Built with TypeScript, Framer Motion and PWA, this app is
+          perfect for practicing your skills while staying organized.
+        </p>
+      </section>
     </main>
   );
 }
