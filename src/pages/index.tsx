778bf0a2ff6f2e503877fbdc06ac9153ab30f325
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-06 17:43:37 +0200

    Created Dark Mode Toggle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import DarkModeToggle from "@/components/darkModeToggle";
+
 export default function Home() {
   return (
     <main>
+      <DarkModeToggle>Dark Mode</DarkModeToggle>
       <h1>Happy Notes</h1>
     </main>
   );
