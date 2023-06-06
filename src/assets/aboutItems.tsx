e0ecaa9217ca2772386b50b52a879b88fc090c98
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 01:10:13 +0200

    Moved about items to separate file

---
+++ src/assets/aboutItems.tsx
@@ -0,0 +1,5 @@
+export const AboutItems = {
+  title: "About",
+  description:
+    "To-do list app designed for productivity enthusiasts. With seamless functionality, this application allows users to effortlessly create and manage their tasks. To delete an item, simply swipe right across the screen. Built with TypeScript, Framer Motion and PWA, this app is perfect for practicing your skills while staying organized.",
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,6 @@
+import { AboutItems } from "@/assets/aboutItems";
+
 export default function About() {
-  const AboutItems = {
-    title: "About",
-    description:
-      "To-do list app designed for productivity enthusiasts. With seamless functionality, this application allows users to effortlessly create and manage their tasks. To delete an item, simply swipe right across the screen. Built with TypeScript, Framer Motion and PWA, this app is perfect for practicing your skills while staying organized.",
-  };
   const { title, description } = AboutItems;
   return (
     <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 transition-all bg-slate-100 dark:bg-gray-900">
