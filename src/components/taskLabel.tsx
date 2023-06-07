7425a65541fb247f2c6e1e5a95133d22f3a1ee47
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 21:28:43 +0200

    Styled task label

--- src/components/taskLabel.tsx
+++ src/components/taskLabel.tsx
@@ -1,7 +1,30 @@
-export default function TaskLabel() {
+import { motion } from "framer-motion";
+
+interface TaskProps {
+  children: React.ReactNode;
+  isDone: boolean;
+}
+
+export default function TaskLabel({ children, isDone }: TaskProps) {
   return (
     <div>
-      <h1>TaskLabel</h1>
+      <motion.label
+        className={`${
+          isDone
+            ? "text-gray-600 dark:text-gray-400"
+            : "text-gray-900 dark:text-white "
+        } relative ml-3 cursor-grab transition-all`}
+      >
+        {children}
+        <motion.span
+          className={` ${
+            isDone ? "bg-gray-600 dark:bg-gray-400" : "bg-black"
+          } absolute left-0 w-full h-0.5 translate-y-1/2 top-1/2`}
+          initial={{ width: 0 }}
+          animate={{ width: isDone ? "100%" : 0 }}
+          transition={{ duration: 0.1 }}
+        ></motion.span>
+      </motion.label>
     </div>
   );
 }
