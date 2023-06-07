db0ea58a472ca42d234b323fcb278f414ed6d5e3
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 21:34:56 +0200

    Styled task

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,7 +1,35 @@
-export default function Task() {
+import { useState } from "react";
+import CheckBox from "./checkBox";
+import TaskLabel from "./taskLabel";
+import { motion } from "framer-motion";
+
+interface TaskProps {
+  task: { title: string; done: boolean };
+}
+
+export default function Task({ task }: TaskProps) {
+  function handleDragEnd(event: any, info: any) {
+    const offset = info.offset.x;
+    const velocity = info.velocity.x;
+  }
   return (
-    <div>
-      <h1>Task</h1>
-    </div>
+    <motion.span
+      className="relative flex items-center justify-between w-48 h-8 pl-4 my-3 overflow-hidden cursor-grab"
+      layout
+      drag="x"
+      dragConstraints={{ left: 0, right: 0 }}
+      onDragEnd={handleDragEnd}
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8 }}
+      transition={{ opacity: { duration: 0.2 } }}
+    >
+      <motion.span className="flex items-center">
+        <span>
+          <CheckBox isDone={task.done} />
+        </span>
+        <TaskLabel isDone={task.done}>{task.title}</TaskLabel>
+      </motion.span>
+    </motion.span>
   );
 }
--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,9 +1,12 @@
 import AddNew from "@/components/addNew";
+import Task from "@/components/task";
 
 export default function ToDo() {
+  const task = { title: "test", done: false };
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
-      <h1>ToDo</h1>
+      <Task task={task} />
+
       <AddNew />
     </main>
   );
