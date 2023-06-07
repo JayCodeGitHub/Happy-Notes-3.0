aee13b9bebaedaee3e7fc040482943b079abb353
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 15:01:05 +0200

    Styled add new for mobile

--- src/components/addNew.tsx
+++ src/components/addNew.tsx
@@ -1,6 +1,6 @@
 export default function AddNew() {
   return (
-    <button className="fixed w-16 h-16 bg-green-500 rounded-full bottom-6 right-6">
+    <button className="fixed bg-green-500 rounded-full md:w-16 md:h-16 h-14 w-14 bottom-6 right-6">
       <span className="absolute z-10 w-1 h-8 rotate-90 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
       <span className="absolute w-1 h-8 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
     </button>
