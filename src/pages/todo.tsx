f5935dd5877c94fe8939074f0a6024d284196215
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 21:36:19 +0200

    Styled todo section

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -4,9 +4,10 @@ import Task from "@/components/task";
 export default function ToDo() {
   const task = { title: "test", done: false };
   return (
-    <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
-      <Task task={task} />
-
+    <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 py-16 transition-all bg-slate-100 dark:bg-gray-900">
+      <section className="flex flex-col items-start w-full mt-5 md:w-60 px-14 md:px-0">
+        <Task task={task} />
+      </section>
       <AddNew />
     </main>
   );
