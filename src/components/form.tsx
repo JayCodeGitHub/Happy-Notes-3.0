fafdcd0ed4fd25cfde7054edd5caa310d03f3bf5
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 21:48:24 +0200

    Styled form for mobile

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,6 @@
 export default function Form() {
   return (
-    <form className="absolute text-gray-900 -translate-x-1/2 -translate-y-1/2 bg-white w-[35vw] rounded-2xl top-[40%] left-1/2 flex flex-col px-4 py-4 drop-shadow-lg z-30">
+    <form className="absolute text-gray-900 -translate-x-1/2 -translate-y-1/2 bg-white md:w-[35vw] w-10/12 rounded-2xl md:top-[40%] top-56 left-1/2 flex flex-col px-4 py-4 drop-shadow-lg z-30">
       <label className="mb-1 ml-2">title</label>
       <input
         type="text"
