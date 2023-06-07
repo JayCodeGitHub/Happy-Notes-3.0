f6265e5a259e47e698d03b9bd73097dea4a248e0
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 14:47:11 +0200

    Added Add new to all pages

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,7 +1,10 @@
+import AddNew from "@/components/addNew";
+
 export default function ToDo() {
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <h1>ToDo</h1>
+      <AddNew />
     </main>
   );
 }
