accfba2f4f87ae0d83b3ce4756981cef4be709d3
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 14:46:24 +0200

    Styled Add New

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,7 +1,10 @@
+import AddNew from "@/components/addNew";
+
 export default function Notes() {
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <h1>Notes</h1>
+      <AddNew />
     </main>
   );
 }
