97b00107e349d0cf34abedee0dd0372af78bef68
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 15:17:36 +0200

    Added form to add new

--- src/components/addNew.tsx
+++ src/components/addNew.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import Form from "./form";
+
 export default function AddNew() {
+  const [visible, setVisible] = useState(false);
   return (
-    <button className="fixed bg-green-500 rounded-full md:w-16 md:h-16 h-14 w-14 bottom-6 right-6">
-      <span className="absolute z-10 w-1 h-8 rotate-90 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
-      <span className="absolute w-1 h-8 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
-    </button>
+    <>
+      <button
+        className="fixed bg-green-500 rounded-full md:w-16 md:h-16 h-14 w-14 bottom-6 right-6"
+        onClick={() => setVisible(true)}
+      >
+        <span className="absolute z-10 w-1 h-8 rotate-90 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
+        <span className="absolute w-1 h-8 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
+      </button>
+      {visible ? (
+        <span onClick={() => setVisible(false)}>
+          <Form />
+        </span>
+      ) : null}
+    </>
   );
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,6 @@
 export default function Form() {
   return (
-    <form>
+    <form className="absolute h-96 text-white -translate-x-1/2 -translate-y-1/2 bg-gray-900 w-[40vw] rounded-2xl top-1/3 left-1/2">
       <h1>Form</h1>
     </form>
   );
