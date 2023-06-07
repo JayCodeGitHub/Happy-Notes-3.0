0e304d19f10162da31447e87e74f6afda37913e1
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-07 22:23:45 +0200

    Styled Notes section

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -4,7 +4,9 @@ import Note from "@/components/note";
 export default function Notes() {
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
-      <Note />
+      <section className="absolute grid gap-12 px-8 mt-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2">
+        <Note />
+      </section>
       <AddNew type="note" />
     </main>
   );
