f9477afa4c092c98f59dc3c473f435effb6bcb60
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 01:46:07 +0200

    Added accessible name to add new button

--- src/components/addNew.tsx
+++ src/components/addNew.tsx
@@ -10,8 +10,9 @@ export default function AddNew({ type }: AddNewProps) {
   return (
     <>
       <button
-        className="fixed transition-all bg-green-500 rounded-full md:w-16 md:h-16 h-14 w-14 bottom-6 right-6 hover:bg-green-600"
+        aria-label="Add New Item"
         onClick={() => setVisible(true)}
+        className="fixed transition-all bg-green-500 rounded-full md:w-16 md:h-16 h-14 w-14 bottom-6 right-6 hover:bg-green-600"
       >
         <span className="absolute z-10 w-1 h-8 rotate-90 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
         <span className="absolute w-1 h-8 -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm top-1/2 left-1/2"></span>
