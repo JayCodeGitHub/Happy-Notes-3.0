8c4c897bd054e52f89e7c18f8b5e05fcc53e2cbc
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 01:10:41 +0200

    Fix note animation

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -13,7 +13,7 @@ export default function Note({ title, body }: NoteProps) {
   const { removeItem } = bindActionCreators(actionCreators, dispatch);
   return (
     <motion.div
-      className="overflow-hidden transition-all rounded-md dark:bg-gray-700 bg-slate-200 h-96"
+      className="overflow-hidden transition-colors rounded-md dark:bg-gray-700 bg-slate-200 h-96"
       layout
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -23,13 +23,13 @@ export default function Note({ title, body }: NoteProps) {
       <div className="flex items-center justify-between h-16 px-4 bg-green-500">
         <h1 className="text-lg text-white truncate">{title}</h1>
         <button
-          className="px-4 py-2 ml-1 text-gray-900 transition-all rounded-full dark:text-white dark:bg-gray-700 bg-slate-200 dark:hover:bg-gray-500 hover:bg-slate-300"
+          className="px-4 py-2 ml-1 text-gray-900 transition-colors rounded-full dark:text-white dark:bg-gray-700 bg-slate-200 dark:hover:bg-gray-500 hover:bg-slate-300"
           onClick={() => removeItem(title, "notes")}
         >
           Remove
         </button>
       </div>
-      <p className="m-4 overflow-hidden text-base text-gray-900 transition-all dark:text-white h-72">
+      <p className="m-4 overflow-hidden text-base text-gray-900 transition-colors dark:text-white h-72">
         {body}
       </p>
     </motion.div>
