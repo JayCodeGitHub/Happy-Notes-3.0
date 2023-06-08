7d10508638ec2ab7a6706cfd77ed252519782c1e
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 01:10:08 +0200

    Fix site animations

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -13,25 +13,25 @@ export default function Site({ title, url }: SiteProps) {
   const { removeItem } = bindActionCreators(actionCreators, dispatch);
   return (
     <motion.div
-      className="p-2 overflow-hidden text-center transition-all rounded-md dark:bg-gray-700 bg-slate-200"
+      className="p-2 overflow-hidden text-center transition-colors rounded-md dark:bg-gray-700 bg-slate-200"
       layout
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.8 }}
       transition={{ opacity: { duration: 0.2 } }}
     >
-      <h1 className="text-xl text-gray-900 transition-all dark:text-white">
+      <h1 className="text-xl text-gray-900 transition-colors dark:text-white">
         {title}
       </h1>
       <span className="flex justify-around w-full mt-2">
         <a href={url} target="_blank">
-          <button className="px-4 py-2 ml-1 text-white transition-all bg-green-500 rounded-full hover:bg-green-600">
+          <button className="px-4 py-2 ml-1 text-white transition-colors bg-green-500 rounded-full hover:bg-green-600">
             Go to site
           </button>
         </a>
         <button
           onClick={() => removeItem(title, "sites")}
-          className="px-4 py-2 ml-1 text-white transition-all bg-gray-400 rounded-full hover:bg-gray-500"
+          className="px-4 py-2 ml-1 text-white transition-colors bg-gray-400 rounded-full hover:bg-gray-500"
         >
           Remove
         </button>
