3a16dd75e477c5d0b1bf284a9c5c68c08becef19
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 15:17:57 +0200

    Integrated notes with useNotes

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,12 +1,14 @@
 import AddNew from "@/components/addNew";
 import Note from "@/components/note";
 import { NotesItems } from "@/assets/notesItems";
+import { useNotes } from "@/hooks/useNotes";
 
 export default function Notes() {
+  const { notes } = useNotes();
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <section className="absolute grid w-full gap-12 px-8 mt-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2">
-        {NotesItems.map(({ title, body }) => (
+        {notes.map(({ title, body }) => (
           <Note key={title} title={title} body={body} />
         ))}
       </section>
