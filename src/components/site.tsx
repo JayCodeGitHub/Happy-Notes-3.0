929ba041ad81ab28227c6b6299486b8e556a0862
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 13:48:16 +0200

    Styled site

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -1,13 +1,15 @@
 export default function Site() {
   return (
-    <div className="overflow-hidden bg-gray-700 rounded-md">
-      <button className="px-4 py-2 ml-1 text-white transition-all bg-green-500 rounded-full hover:bg-green-600">
-        Go to site
-      </button>
-      <button className="px-4 py-2 ml-1 text-white transition-all bg-gray-400 rounded-full hover:bg-gray-500">
-        Remove
-      </button>
-      <h1>Site</h1>
+    <div className="p-2 overflow-hidden text-center bg-gray-700 rounded-md">
+      <h1 className="text-xl text-white">Site Name</h1>
+      <span className="flex justify-around w-full mt-2">
+        <button className="px-4 py-2 ml-1 text-white transition-all bg-green-500 rounded-full hover:bg-green-600">
+          Go to site
+        </button>
+        <button className="px-4 py-2 ml-1 text-white transition-all bg-gray-400 rounded-full hover:bg-gray-500">
+          Remove
+        </button>
+      </span>
     </div>
   );
 }
