f3251890b54ce9dfaff6fd2c63552b95748719be
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 01:08:13 +0200

    Added new colors to site

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -13,14 +13,16 @@ export default function Site({ title, url }: SiteProps) {
   const { removeItem } = bindActionCreators(actionCreators, dispatch);
   return (
     <motion.div
-      className="p-2 overflow-hidden text-center bg-gray-700 rounded-md"
+      className="p-2 overflow-hidden text-center transition-all rounded-md dark:bg-gray-700 bg-slate-200"
       layout
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.8 }}
       transition={{ opacity: { duration: 0.2 } }}
     >
-      <h1 className="text-xl text-white">{title}</h1>
+      <h1 className="text-xl text-gray-900 transition-all dark:text-white">
+        {title}
+      </h1>
       <span className="flex justify-around w-full mt-2">
         <a href={url} target="_blank">
           <button className="px-4 py-2 ml-1 text-white transition-all bg-green-500 rounded-full hover:bg-green-600">
