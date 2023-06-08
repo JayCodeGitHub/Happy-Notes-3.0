b2e5d5ce851a57c18e218b56ba0cc5acc06779fd
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 14:08:04 +0200

    Moved sites items to separate file

---
+++ src/assets/sitesItems.tsx
@@ -0,0 +1,4 @@
+export const SitesItems = [
+  { title: "Facebook", url: "https://www.facebook.com/" },
+  { title: "Youtube", url: "https://www.youtube.com/" },
+];
--- src/pages/sites.tsx
+++ src/pages/sites.tsx
@@ -1,11 +1,8 @@
 import AddNew from "@/components/addNew";
 import Site from "@/components/site";
+import { SitesItems } from "@/assets/sitesItems";
 
 export default function Sites() {
-  const SitesItems = [
-    { title: "Facebook", url: "https://www.facebook.com/" },
-    { title: "Youtube", url: "https://www.youtube.com/" },
-  ];
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <section className="absolute grid w-full gap-12 px-8 mt-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2">
