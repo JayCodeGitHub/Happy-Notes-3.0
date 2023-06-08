6d321851c2d8e4c3a2885654db6ef802064bf2e3
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 14:04:37 +0200

    Refactored site

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -1,7 +1,12 @@
-export default function Site() {
+interface SiteProps {
+  title: string;
+  url: string;
+}
+
+export default function Site({ title, url }: SiteProps) {
   return (
     <div className="p-2 overflow-hidden text-center bg-gray-700 rounded-md">
-      <h1 className="text-xl text-white">Site Name</h1>
+      <h1 className="text-xl text-white">{title}</h1>
       <span className="flex justify-around w-full mt-2">
         <button className="px-4 py-2 ml-1 text-white transition-all bg-green-500 rounded-full hover:bg-green-600">
           Go to site
--- src/pages/sites.tsx
+++ src/pages/sites.tsx
@@ -2,12 +2,16 @@ import AddNew from "@/components/addNew";
 import Site from "@/components/site";
 
 export default function Sites() {
+  const SitesItems = [
+    { title: "Facebook", url: "https://www.facebook.com/" },
+    { title: "Youtube", url: "https://www.youtube.com/" },
+  ];
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <section className="absolute grid w-full gap-12 px-8 mt-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2">
-        <Site />
-        <Site />
-        <Site />
+        {SitesItems.map(({ title, url }) => (
+          <Site key={title} title={title} url={url} />
+        ))}
       </section>
       <AddNew type="site" />
     </main>
