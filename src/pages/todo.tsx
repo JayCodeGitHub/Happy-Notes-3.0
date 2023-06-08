a850a17333f0d622a77c1f21e14c7328d0f87133
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 14:00:02 +0200

    Refactored todo site

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -2,11 +2,16 @@ import AddNew from "@/components/addNew";
 import Task from "@/components/task";
 
 export default function ToDo() {
-  const task = { title: "test", done: false };
+  const todoItems = [
+    { title: "test", done: false },
+    { title: "test", done: false },
+  ];
   return (
     <main className="absolute flex flex-col items-center justify-start w-full h-screen gap-5 py-16 transition-all bg-slate-100 dark:bg-gray-900">
       <section className="flex flex-col items-start w-full mt-5 md:w-60 px-14 md:px-0">
-        <Task task={task} />
+        {todoItems.map((item) => (
+          <Task task={item} key={item.title} />
+        ))}
       </section>
       <AddNew type="todo" />
     </main>
