0ce7b1fd8c09f7b19ccca54e84e91252cb43266a
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 15:40:14 +0200

    Integrated sites with useSites

--- src/pages/sites.tsx
+++ src/pages/sites.tsx
@@ -1,12 +1,13 @@
 import AddNew from "@/components/addNew";
 import Site from "@/components/site";
-import { SitesItems } from "@/assets/sitesItems";
+import { useSites } from "@/hooks/useSites";
 
 export default function Sites() {
+  const { sites } = useSites();
   return (
     <main className="min-h-screen text-gray-900 transition-all bg-slate-100 dark:bg-gray-900 dark:text-white">
       <section className="absolute grid w-full gap-12 px-8 mt-8 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2">
-        {SitesItems.map(({ title, url }) => (
+        {sites.map(({ title, url }) => (
           <Site key={title} title={title} url={url} />
         ))}
       </section>
