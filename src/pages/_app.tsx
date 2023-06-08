336f5fe0fa25d2c06aace2cc809680a53843434f
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-08 15:11:05 +0200

    Setup useNotes hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
 import { TasksProvider } from "@/hooks/useTasks";
+import { NotesProvider } from "@/hooks/useNotes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <TasksProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </TasksProvider>
+    <NotesProvider>
+      <TasksProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TasksProvider>
+    </NotesProvider>
   );
 }
