f9ada89d4a2dedb466b08d4561ff6dbe7933d3f2
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 13:39:03 +0200

    Fix form type validation

--- src/components/addNew.tsx
+++ src/components/addNew.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Form from "./form";
 
 interface AddNewProps {
-  type: string;
+  type: "notes" | "sites" | "todos";
 }
 
 export default function AddNew({ type }: AddNewProps) {
