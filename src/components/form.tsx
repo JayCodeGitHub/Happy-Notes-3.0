47e4a910341c62ae8d537f3d07f600a4965d43e8
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 13:36:15 +0200

    Added form validation

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,18 +1,20 @@
 import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
-import { actionCreators } from "@/state";
+import { actionCreators, State } from "@/state";
 
 interface FormProps {
-  type: string;
+  type: "notes" | "sites" | "todos";
   setVisible: (state: boolean) => void;
 }
 
 export default function Form({ type, setVisible }: FormProps) {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const state = useSelector((state: State) => state);
   const dispatch = useDispatch();
   const { addItem } = bindActionCreators(actionCreators, dispatch);
+  const [error, setError] = useState<boolean | string>(false);
   const [form, setForm] = useState({
     title: "",
     body: "",
@@ -30,10 +32,27 @@ export default function Form({ type, setVisible }: FormProps) {
     });
   };
 
+  const validate = (title: string) => {
+    let valid = true;
+    state[type].map(function (item) {
+      if (item.title === title) {
+        setError(`The ${type} already exists`);
+        valid = false;
+      }
+    });
+    if (valid) return null;
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addItem(type, form.title, form.body);
-    setVisible(false);
+
+    if (form.title.length < 1) {
+      setError("Type a title");
+    } else if (validate(form.title) === null) {
+      addItem(type, form.title, form.body);
+      setError(false);
+      setVisible(false);
+    }
   };
 
   useEffect(() => {
@@ -51,6 +70,7 @@ export default function Form({ type, setVisible }: FormProps) {
       transition={{ opacity: { duration: 0.2 } }}
       className="absolute text-gray-900 -translate-x-1/2 -translate-y-1/2 bg-white md:w-[35vw] w-10/12 rounded-2xl md:top-[40%] top-56 left-1/2 flex flex-col px-4 py-4 drop-shadow-lg z-30"
     >
+      {error ? <p className="text-red-500 transition-all">{error}</p> : null}
       <label className="mb-1 ml-2">title</label>
       <input
         ref={inputRef}
