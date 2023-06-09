5341c32b5858c3cf3bb0d2764614fddb3a401abd
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-09 02:02:16 +0200

    Updated head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Happy Notes</title>
         <meta
           name="description"
-          content="Happy Notes is a feature-rich note-taking application built with Next.js, React, Framer Motion, Tailwind CSS, and Redux. 
+          content="Happy Notes is a feature-rich note-taking application built with Next.js, React, Framer Motion, Tailwind CSS, and Redux. It is also integrated with Progressive Web App (PWA) technology.
           It offers a user-friendly interface for creating and organizing three different types of notes. With local storage integration, 
           your notes are securely stored on the user's device. Seamlessly designed with an intuitive layout, Happy Notes enhances productivity and streamlines your note-taking experience. 
           Whether it's personal memos, brainstorming sessions, or task lists, this app is a perfect companion for organizing your thoughts and staying productive."
